dc005240b26c3209ed45fa082ecc0a7ee6d6548e
Owen Davies <owen@odavi.es>
2021-01-10 10:48:50 +0000

    Add Day 8

---
+++ src/main/scala/advent2016/Day8.scala
@@ -0,0 +1,41 @@
+package advent2016
+
+import scalaadventutils.Problem
+
+import scala.collection.mutable.ArrayBuffer
+
+object Day8 {
+
+    val rect = """^rect\s+(\d+)x(\d+)$""".r
+    val row  = """^rotate\s+row\s+y=(\d+)\s+by\s+(\d+)$""".r
+    val col  = """^rotate\s+column\s+x=(\d+)\s+by\s+(\d+)$""".r
+
+    def main(args: Array[String]) {
+        val input = Problem.parseInputToList("day8")
+        println(part1(input, 50, 6))
+        println(part2(input, 50, 6).toString())
+    }
+
+    def part1(input: List[String], width: Int, height: Int) =
+        runCommands(input, width, height).countOn()
+
+    def part2(input: List[String], width: Int, height: Int) =
+        runCommands(input, width, height)
+
+    def parseLine(line: String, grid: Grid) = line match {
+        case rect(w, h) => (x: Int, y: Int) =>
+            if (x < w.toInt && y < h.toInt) true
+            else grid.get(x, y)
+        case row(r, shift) => (x: Int, y: Int) =>
+            if (y == r.toInt) grid.get(x - shift.toInt, y)
+            else grid.get(x, y)
+        case col(c, shift) => (x: Int, y: Int) =>
+            if (x == c.toInt) grid.get(x, y - shift.toInt)
+            else grid.get(x, y)
+    }
+
+    private def runCommands(input: List[String], width: Int, height: Int) =
+        input.foldLeft(
+            new Grid(ArrayBuffer.fill(height * width)(false), width, height)
+        )((next, line) => next.step(parseLine(line, next)))
+}
---
+++ src/test/scala/Day8.scala
@@ -0,0 +1,68 @@
+package advent2016
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day8Spec extends AnyFunSuite {
+
+    val grid = List[String](
+        ".......",
+        ".......",
+        "......."
+    )
+
+    test("Day 8: parse and step") {
+        var g1 = GridUtils.from2DCharArray(grid, '#')
+
+        var line = "rect 3x2"
+        var step = Day8.parseLine(line, g1)
+        g1 = g1.step(step)
+
+        var out = List[String](
+            "###....",
+            "###....",
+            "......."
+        )
+
+        assert(out.mkString("\n") == g1.toString().trim())
+
+        line = "rotate column x=1 by 1"
+        step = Day8.parseLine(line, g1)
+        g1 = g1.step(step)
+
+        out = List[String](
+            "#.#....",
+            "###....",
+            ".#....."
+        )
+
+        println(out.mkString("\n"))
+        println()
+        println(g1.toString())
+
+        assert(out.mkString("\n") == g1.toString().trim())
+
+        line = "rotate row y=0 by 4"
+        step = Day8.parseLine(line, g1)
+        g1 = g1.step(step)
+
+        out = List[String](
+            "....#.#",
+            "###....",
+            ".#....."
+        )
+
+        assert(out.mkString("\n") == g1.toString().trim())
+
+        line = "rotate column x=1 by 1"
+        step = Day8.parseLine(line, g1)
+        g1 = g1.step(step)
+
+        out = List[String](
+            ".#..#.#",
+            "#.#....",
+            ".#....."
+        )
+
+        assert(out.mkString("\n") == g1.toString().trim())
+    }
+}
